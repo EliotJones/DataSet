674bfba10a0eebc846c4cb8c9761d31b8fdbad57
Eliot Jones <elioty@hotmail.co.uk>
2014-12-07 17:12:47 +0000

    Added Enums for Settings Provider and StyleCop integration/settings

---
+++ EliotJones.DataSet/DataTableParserSettings.cs
@@ -0,0 +1,34 @@
+﻿namespace EliotJones.DataSet
+{
+    using EliotJones.DataSet.Enums;
+
+    /// <summary>
+    /// Settings for de-serializing a DataTable.
+    /// </summary>
+    public class DataTableParserSettings
+    {
+        private const MissingMappingHandling DefaultMissingMappingHandling = MissingMappingHandling.Ignore;
+        private const MappingMatchOrder DefaultMappingMatchOrder = MappingMatchOrder.PropertyNameFirst;
+
+        private MissingMappingHandling? missingMappingHandling;
+        private MappingMatchOrder? mappingMatchOrder;
+
+        /// <summary>
+        /// Gets or sets the handling of an incomplete DataTable to Class mapping.
+        /// </summary>
+        public MissingMappingHandling MissingMappingHandling 
+        {
+            get { return missingMappingHandling ?? DefaultMissingMappingHandling; }
+            set { missingMappingHandling = value; } 
+        }
+
+        /// <summary>
+        /// Gets or sets the order of importance with which property and attribute names are treated.
+        /// </summary>
+        public MappingMatchOrder MappingMatchOrder 
+        {
+            get { return mappingMatchOrder ?? DefaultMappingMatchOrder; }
+            set { mappingMatchOrder = value; } 
+        }
+    }
+}
---
+++ EliotJones.DataSet/Enums/MappingMatchOrder.cs
@@ -0,0 +1,11 @@
+﻿namespace EliotJones.DataSet.Enums
+{
+    /// <summary>
+    /// Sets whether the property name or the attribute value should take precedence in mapping columns to properties.
+    /// </summary>
+    public enum MappingMatchOrder
+    {
+        PropertyNameFirst = 1,
+        AttributeValueFirst = 2
+    }
+}
