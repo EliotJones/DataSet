322c0f28bcc1927662e950c5132fdc2c54853566
Eliot Jones <elioty@hotmail.co.uk>
2015-02-07 16:12:11 +0000

    Refactored PropertyResolver to have more, smaller, methods

--- EliotJones.DataSet/MappingResolvers/PropertyResolverHelper.cs
+++ EliotJones.DataSet/MappingResolvers/PropertyResolverHelper.cs
@@ -2,6 +2,7 @@
 {
     using System.Collections.Generic;
     using System.Data;
+    using System.Globalization;
     using System.Linq;
     using System.Reflection;
 
@@ -30,22 +31,20 @@
                 // Special case handling for Id columns/properties.
                 if (!mappingFound && property.Name.ToLowerInvariant().Contains(id))
                 {
-                    string searchTerm = null;
-
-                    if (property.Name.ToLowerInvariant() == id)
-                    {
-                        searchTerm = property.DeclaringType.Name + id;
-                    }
-                    else
-                    {
-                        searchTerm = id;
-                    }
-
-                    if (mappingObjects.DataTable.Columns.Contains(searchTerm))
-                    {
-                        mappedProperties.Add(new ExtendedPropertyInfo(fieldName: searchTerm, propertyInfo: property, columnIndex: mappingObjects.DataTable.Columns.IndexOf(searchTerm)));
-                    }
+                    GenerateIdSpecificPropertyMappings(ref mappedProperties, mappingObjects, property, id);
                 }
+            }
+        }
+
+        private void GenerateIdSpecificPropertyMappings(ref List<ExtendedPropertyInfo> mappedProperties, MappingObjects mappingObjects, PropertyInfo property, string id)
+        {
+            string searchTerm = null;
+
+            searchTerm = (string.Compare(property.Name, id, true, CultureInfo.InvariantCulture) == 0) ? property.DeclaringType.Name + id : id;
+
+            if (mappingObjects.DataTable.Columns.Contains(searchTerm))
+            {
+                mappedProperties.Add(new ExtendedPropertyInfo(fieldName: searchTerm, propertyInfo: property, columnIndex: mappingObjects.DataTable.Columns.IndexOf(searchTerm)));
             }
         }
     }
