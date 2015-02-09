369340d6d9615a800d95c8c2b7bc3095a4623fe0
Eliot Jones <elioty@hotmail.co.uk>
2015-02-09 19:08:20 +0000

    Begin ValueTypeConverter Implementation

--- EliotJones.DataTable.Tests.Unit/Tests/DataTypeConverters/DefaultDataTypeConverterTests.cs
+++ EliotJones.DataTable.Tests.Unit/Tests/DataTypeConverters/DefaultDataTypeConverterTests.cs
@@ -16,19 +16,19 @@
             get
             {
                 return new[]
-            {
-                new object[] { string.Empty },
-                new object[] { null },
-                new object[] { 'x' },
-                new object[] { '\r' },
-                new object[] { "string" },
-                new object[] { new DateTime(2001, 1, 1) },
-                new object[] { int.MinValue },
-                new object[] { int.MaxValue },
-                new object[] { 0.5454f },
-                new object[] { DBNull.Value },
-                new object[] { Guid.Empty }
-            };
+                {
+                    new object[] { string.Empty },
+                    new object[] { null },
+                    new object[] { 'x' },
+                    new object[] { '\r' },
+                    new object[] { "string" },
+                    new object[] { new DateTime(2001, 1, 1) },
+                    new object[] { int.MinValue },
+                    new object[] { int.MaxValue },
+                    new object[] { 0.5454f },
+                    new object[] { DBNull.Value },
+                    new object[] { Guid.Empty }
+                };
             }
         }
 
@@ -37,17 +37,47 @@
             get
             {
                 return new[]
+                {
+                    new object[] { string.Empty },
+                    new object[] { 'x' },
+                    new object[] { '\r' },
+                    new object[] { "string" },
+                    new object[] { new DateTime(2001, 1, 1) },
+                    new object[] { int.MinValue },
+                    new object[] { int.MaxValue },
+                    new object[] { 0.5454f },
+                    new object[] { Guid.Empty }
+                };
+            }
+        }
+
+        public static IEnumerable<object[]> IntTestDataGood
+        {
+            get
             {
-                new object[] { string.Empty },
-                new object[] { 'x' },
-                new object[] { '\r' },
-                new object[] { "string" },
-                new object[] { new DateTime(2001, 1, 1) },
-                new object[] { int.MinValue },
-                new object[] { int.MaxValue },
-                new object[] { 0.5454f },
-                new object[] { Guid.Empty }
-            };
+                return new[]
+                {
+                    new object[] { int.MinValue },
+                    new object[] { int.MaxValue },
+                    new object[] { 0 },
+                    new object[] { "1" },
+                    new object[] { "-1" },
+                    new object[] { 1.0 },
+                    new object[] { 1.3 }
+                };
+            }
+        }
+
+        public static IEnumerable<object[]> IntTestDataBad
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { "az" },
+                    new object[] { new DateTime(2001, 1, 1) },
+                    new object[] { Guid.Empty }
+                };
             }
         }
 
@@ -73,6 +103,28 @@
             var converter = new DefaultDataTypeConverter();
 
             Assert.Throws<NotImplementedException>(() => converter.FieldToObject(input, typeof(StringBuilder), defaultSettings, new DbNullConverter(defaultSettings)));
+        }
+
+        [Theory]
+        [PropertyData("IntTestDataGood")]
+        public void FieldToObject_SupportedTypeToInt_ReturnsInt(object toConvert)
+        {
+            var converter = new DefaultDataTypeConverter();
+
+            object result = converter.FieldToObject(toConvert, typeof(int), defaultSettings, new DbNullConverter(defaultSettings));
+
+            int expectedResult = Convert.ToInt32(toConvert);
+
+            Assert.Equal(expectedResult, (int)result);
+        }
+
+        [Theory]
+        [PropertyData("IntTestDataBad")]
+        public void FieldToObject_UnsupportedTypeToInt_ThrowsNotImplementedException(object toConvert)
+        {
+            var converter = new DefaultDataTypeConverter();
+
+            Assert.Throws<NotImplementedException>(() => converter.FieldToObject(toConvert, typeof(int), defaultSettings, new DbNullConverter(defaultSettings)));
         }
     }
 }
--- EliotJones.DataTable/DataTypeConverter/DefaultDataTypeConverter.cs
+++ EliotJones.DataTable/DataTypeConverter/DefaultDataTypeConverter.cs
@@ -7,9 +7,9 @@
     {
         private Type stringType = typeof(string);
 
-        public virtual object FieldToObject(object field, 
-            Type type, 
-            DataTableParserSettings settings, 
+        public virtual object FieldToObject(object field,
+            Type type,
+            DataTableParserSettings settings,
             DbNullConverter dbNullConverter)
         {
             if (field == DBNull.Value || field == null)
@@ -27,12 +27,44 @@
                 throw new NotImplementedException("No Conversion exists for class of type: " + type.Name);
             }
 
-            return ValueTypeFieldToObject(field, type, settings);            
+            return ValueTypeFieldToObject(field, type, settings);
         }
 
         protected virtual object ValueTypeFieldToObject(object field, Type type, DataTableParserSettings settings)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (type == typeof(int))
+                {
+                    if (field is int)
+                    {
+                        return field;
+                    }
+                    else if (field is string)
+                    {
+                        int returnValue;
+
+                        bool canParse = int.TryParse(field.ToString(), out returnValue);
+
+                        if (!canParse)
+                        {
+                            throw new NotImplementedException(string.Format("Cannot convert string: {0} to int", field.ToString()));
+                        }
+
+                        return returnValue;
+                    }
+                    else
+                    {
+                        return Convert.ToInt32(field);
+                    }
+                }
+
+                throw new NotImplementedException(string.Format("No conversion for field with value: {0} to type: {1}", field.ToString(), type.Name));
+            }
+            catch (InvalidCastException ex)
+            {
+                throw new NotImplementedException(string.Format("No conversion for field with value: {0} to type: {1}", field.ToString(), type.Name), ex);
+            }
         }
     }
 }
