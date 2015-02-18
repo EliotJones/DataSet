1b06e54a0c23961f65524794fad418ea3b26d85d
Eliot Jones <elioty@hotmail.co.uk>
2015-02-18 18:46:06 +0000

    Added some more converters for value types.

--- EliotJones.DataTable/DataTypeConverter/DefaultDataTypeConverter.cs
+++ EliotJones.DataTable/DataTypeConverter/DefaultDataTypeConverter.cs
@@ -36,27 +36,15 @@
             {
                 if (type == typeof(int))
                 {
-                    if (field is int)
-                    {
-                        return field;
-                    }
-                    else if (field is string)
-                    {
-                        int returnValue;
-
-                        bool canParse = int.TryParse(field.ToString(), out returnValue);
-
-                        if (!canParse)
-                        {
-                            throw new NotImplementedException(string.Format("Cannot convert string: {0} to int", field.ToString()));
-                        }
-
-                        return returnValue;
-                    }
-                    else
-                    {
-                        return Convert.ToInt32(field);
-                    }
+                    return FieldToInt(field);
+                }
+                else if (type == typeof(Guid))
+                {
+                    return FieldToGuid(field);
+                }
+                else if (type == typeof(DateTime))
+                {
+                    return FieldToDateTime(field);
                 }
 
                 throw new NotImplementedException(string.Format("No conversion for field with value: {0} to type: {1}", field.ToString(), type.Name));
@@ -64,6 +52,81 @@
             catch (InvalidCastException ex)
             {
                 throw new NotImplementedException(string.Format("No conversion for field with value: {0} to type: {1}", field.ToString(), type.Name), ex);
+            }
+        }
+
+        protected virtual object FieldToDateTime(object field)
+        {
+            if (field is DateTime)
+            {
+                return field;
+            }
+            else if (field is string)
+            {
+                DateTime returnDateTime;
+
+                if (DateTime.TryParse(field.ToString(), out returnDateTime))
+                {
+                    return returnDateTime;
+                }
+                else
+                {
+                    throw new NotImplementedException(string.Format("No conversion for string with value: {0} to DateTime", field.ToString()));
+                }
+            }
+            else
+            {
+                throw new NotImplementedException(string.Format("No conversion for field with value: {0} to DateTime", field.ToString()));
+            }
+        }
+
+        protected virtual object FieldToInt(object field)
+        {
+            if (field is int)
+            {
+                return field;
+            }
+            else if (field is string)
+            {
+                int returnValue;
+
+                bool canParse = int.TryParse(field.ToString(), out returnValue);
+
+                if (!canParse)
+                {
+                    throw new NotImplementedException(string.Format("Cannot convert string: {0} to int", field.ToString()));
+                }
+
+                return returnValue;
+            }
+            else
+            {
+                return Convert.ToInt32(field);
+            }
+        }
+
+        protected virtual object FieldToGuid(object field)
+        {
+            if (field is Guid)
+            {
+                return field;
+            }
+            else if (field is string)
+            {
+                return Guid.Parse(field.ToString());
+            }
+            else
+            {
+                Guid returnGuid;
+
+                if (Guid.TryParse(field.ToString(), out returnGuid))
+                {
+                    return returnGuid;
+                }
+                else
+                {
+                    throw new NotImplementedException(string.Format("Cannot convert field: {0} to Guid", field.ToString()));
+                }
             }
         }
     }
