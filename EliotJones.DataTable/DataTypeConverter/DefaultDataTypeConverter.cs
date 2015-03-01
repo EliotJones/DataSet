1739296c78dffb6a16781eebd794cedaf0342a45
Eliot Jones <elioty@hotmail.co.uk>
2015-03-01 14:55:10 +0000

    add further conversion to the DataTypeConverter

--- EliotJones.DataTable/DataTypeConverter/DefaultDataTypeConverter.cs
+++ EliotJones.DataTable/DataTypeConverter/DefaultDataTypeConverter.cs
@@ -46,6 +46,36 @@
                 {
                     return FieldToDateTime(field);
                 }
+                else if (type == typeof(bool))
+                {
+                    return FieldToBool(field);
+                }
+                else if (type == typeof(char))
+                {
+                    return FieldToChar(field);
+                }
+                else if (type == typeof(decimal))
+                {
+                    return FieldToDecimal(field);
+                }
+                else if (type == typeof(float))
+                {
+                    return FieldToFloat(field);
+                }
+                else if (type == typeof(uint))
+                {
+                    int value = (int)FieldToInt(field);
+
+                    if (value < 0)
+                    {
+                        value = value * -1;
+                    }
+                    return value;
+                }
+                else if (type == typeof(double))
+                {
+                    return FieldToDouble(field);
+                }
 
                 throw new NotImplementedException(string.Format("No conversion for field with value: {0} to type: {1}", field.ToString(), type.Name));
             }
@@ -127,6 +157,120 @@
                 {
                     throw new NotImplementedException(string.Format("Cannot convert field: {0} to Guid", field.ToString()));
                 }
+            }
+        }
+
+        protected virtual object FieldToBool(object field)
+        {
+            if (field is bool)
+            {
+                return field;
+            }
+            else if (field is int)
+            {
+                return (int)field == 1;
+            }
+            else if (field is string)
+            {
+                bool value = string.Compare(field.ToString(), "true", true) == 0 || field.ToString() == "1";
+
+                return value;
+            }
+            else
+            {
+                bool value;
+
+                if (bool.TryParse(field.ToString(), out value))
+                {
+                    return value;
+                }
+                throw new NotImplementedException(string.Format("Cannot convert field: {0} to bool", field.ToString()));
+            }
+        }
+
+        protected virtual object FieldToChar(object field)
+        {
+            if (field is char)
+            {
+                return field;
+            }
+            else
+            {
+                return field.ToString()[0];
+            }
+        }
+
+        protected virtual object FieldToDecimal(object field)
+        {
+            if (field is decimal)
+            {
+                return field;
+            }
+            else if (field is float || field is double || field is int)
+            {
+                return (decimal)field;
+            }
+            else
+            {
+                decimal result;
+                if (decimal.TryParse(field.ToString(), out result))
+                {
+                    return result;
+                }
+                throw new NotImplementedException(string.Format("Cannot convert field: {0} to decimal", field.ToString()));
+            }
+        }
+
+        protected virtual object FieldToFloat(object field)
+        {
+            if (field is float || field is decimal || field is int)
+            {
+                return field;
+            }
+            else if (field is double)
+            {
+                float result = (float)field;
+
+                if (float.IsPositiveInfinity(result) || float.IsNegativeInfinity(result))
+                {
+                    throw new InvalidCastException("Could not convert double to float without clipping the value");
+                }
+
+                return result;
+            }
+            else
+            {
+                float f;
+                if (float.TryParse(field.ToString(), out f))
+                {
+                    return f;
+                }
+                else
+                {
+                    throw new NotImplementedException(string.Format("Cannot convert field: {0} to Guid", field.ToString()));
+                }
+            }
+        }
+
+        protected virtual object FieldToDouble(object field)
+        {
+            if (field is double 
+                || field is float 
+                || field is decimal 
+                || field is int
+                || field is short
+                || field is long)
+            {
+                return field;
+            }
+            else
+            {
+                double d;
+                if (double.TryParse(field.ToString(), out d))
+                {
+                    return d;
+                }
+                throw new NotImplementedException(string.Format("Cannot convert field: {0} to double", field.ToString()));
             }
         }
     }
